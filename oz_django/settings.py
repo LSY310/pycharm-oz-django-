1b7ffc4ad24c2a3a94ed44e863b306ab6084196b
LIM0310 <limsy0310@gmail.com>
2024-06-19 21:12:55 +0900

    settings.py 문제 해결

--- oz_django/settings.py
+++ oz_django/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-h#$5f4d#g%hw97jlf0ro+inws&=5o@9ds$5%8w0bq*ou*2xvia
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS: list[str] = []
 
 
 # Application definition
